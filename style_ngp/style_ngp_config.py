0a41c44355c3522eb996f349d9f9bbc742f56831
podolskyDavid <david.podolskyi@tum.de>
2024-07-29 18:06:02 -0400

    adapting the training config

--- style_ngp/style_ngp_config.py
+++ style_ngp/style_ngp_config.py
@@ -41,8 +41,8 @@ style_ngp = MethodSpecification(
         pipeline=StyleNGPPipelineConfig(
             datamanager=VanillaDataManagerConfig(
                 dataparser=NerfstudioDataParserConfig(),
-                train_num_rays_per_batch=4096,
-                eval_num_rays_per_batch=4096,
+                train_num_rays_per_batch=8192,
+                eval_num_rays_per_batch=8192,
             ),
             model=StyleNGPModelConfig(eval_num_rays_per_chunk=8192),
         ),
